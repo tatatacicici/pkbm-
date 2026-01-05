2ff7e3f7cf8781020fe1711ed85b6eea98ca3866
fairuz akhdan <fairuzakhdan01@gmail.com>
2026-01-05 11:12:06 +0700

    migration state manajement from recoil to jotai

---
+++ libs/components/src/utils/index.ts
@@ -0,0 +1,7 @@
+// Server-safe utility functions (no 'use client' directive)
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
